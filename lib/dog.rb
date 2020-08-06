1be250b02bfc28d58af2e654c1c655d08d6ffe44
iannakim <dear.hyunji@gmail.com>
2020-08-06 08:43:41 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
 class Dog
+  
+  @@all = []
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def initialize(name_arg, owner_arg)
+    @name = name_arg
+    @owner = owner_arg
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  
   # code goes here
 end
+
+
+
+
+# Define a `Dog` and `Cat` class that have the attributes required by the
+#   test suite. Keep in mind, some of the attributes should be readable and writable
+#   (i.e. `attr_accessor`s), while others may need to be _just_ setters
+#   (`attr_writer`) or _just_ getters (`attr_reader`). The test suite will make this
+#   clear so pay attention to the test output.
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
+require 'pry'
+
+
 class Owner
-  # code goes here
+ 
+  @@all = []
+
+  attr_reader :name, :species, :say_species
+
+  def initialize(name_arg)
+    @name = name_arg
+    @species = "human"
+    @@all << self
+  end
+#binding.pry
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+    
+  end
+
+  def cats
+      Cat.all.select{|cat| cat.owner == self}
+ #returns a a collection of all the cats that belong to the owner
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+#returns a a collection of all the cats that belong to the owner
+  end
+
+  def buy_cat(a_cat)
+    Cat.new(a_cat, self)
+  end
+
+  def buy_dog(a_dog)
+    Dog.new(a_dog, self)
+  end
+
+  def walk_dogs
+    Dog.all.each do |dog|
+      dog.mood ="happy"
+    end
+  end
+  
+  def feed_cats
+    Cat.all.each do |cat|
+      cat.mood ="happy"
+    end
+  end
+
+  def sell_pets
+    all_pets = Cat.all + Dog.all
+    all_pets.each do |pet_info|
+      pet_info.mood = "nervous"
+      pet_info.owner = nil
+    end
+
+  end
+
+    def list_pets
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    end
+
+
+    
 end
+
+#lida = Owner.new("Lida")
